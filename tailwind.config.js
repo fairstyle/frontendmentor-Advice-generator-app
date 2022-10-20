2fb7b9ad38ef44b4430d82421bcd4becdf382619
Andrw <kuvickdabest@gmail.com>
2022-10-20 15:45:19 -0300

    Desktop and mobile design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      // Using modern `hsl`
+      primary: 'hsl(var(--color-primary) / <alpha-value>)',
+      secondary: 'hsl(var(--color-secondary) / <alpha-value>)',
+      grayish: 'hsl(var(--color-grayish) / <alpha-value>)',
+      darkgrayish: 'hsl(var(--color-darkgrayish) / <alpha-value>)',
+      darkblue: 'hsl(var(--color-darkblue) / <alpha-value>)',
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    }
   },
   plugins: [],
 }
